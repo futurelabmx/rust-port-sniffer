0a10c7896788c21a84740057f60d4d69b30e2a21
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-28 15:43:54 -0500

    Added functions (methods) for arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use std::env;
 use std::net::IpAddr;
+use std::str::FromStr;
 
 struct Arguments {
     flag: String,
     ipaddr: IpAddr,
     threads: u16,
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &'static str> {
+        if args.len() < 2 {
+            return Err("No hay suficientes argumentos");
+        } else if args.len() > 4 {
+            return Err("Demasiados argumentos");
+        }
+    }
 }
 
 fn main() {
