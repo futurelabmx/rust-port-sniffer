0aa5fc75977c8912e471ca99800ec30f6eee6f4c
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-28 15:40:51 -0500

    Passed arguments into a structure

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::env;
 use std::net::IpAddr;
 
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-
-    let flag = args[1].clone();
-    let threads = args[2].clone();
-    let ipaddr = args[3].clone();
 }
