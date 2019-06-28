5ac6b9e209dd384ba5d5599d738a4839690030dc
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-28 14:37:31 -0500

    Assigning arguments to variables

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
 
-    for i in &args {
-        println!("{}", i);
-    }
-    println!("{:?}", args);
+    let flag = args[1].clone();
+    let threads = args[2].clone();
+    let ipaddr = args[3].clone();
 }
