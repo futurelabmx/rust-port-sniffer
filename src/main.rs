49560f770179df1959025b5dd97e33a7fc5d2c4d
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-28 16:22:15 -0500

    Added threads arguments handler

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,19 @@ impl Arguments {
                 \r\n      -h o --help para mostrar este mensaje");
                 } else if flag.contains("-h") || flag.contains("--help") {
                     return Err("Demasiados argumentos");
+                } else if flag.contains("-j") {
+                    let ipaddr = match IpAddr::from_str(&args[3]) {
+                        Ok(s) => s,
+                        Err(_) => return Err("No es una dirección IP válida")
+                    };
+
+                    let threads = match args[2].parse::<u16>() {
+                        Ok(s) => s,
+                        Err(_) => return Err("Fallo al leer el número de hilos")
+                    };
+                    return Ok(Arguments{threads, flag, ipaddr});
+                } else {
+                    return Err("Error de Sintáxis");
                 }
         }
     }
