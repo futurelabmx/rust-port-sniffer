eb2ad7fb8b6554ed03797ff58f538b39909b77a7
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-28 14:29:25 -0500

    Testing iterations over arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    for i in &args {
+        println!("{}", i);
+    }
+    println!("{:?}", args);
 }
